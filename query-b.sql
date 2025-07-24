0b85928acebe5a55857791e4f6a47648c0437bf5
Ikechukwu Elijah <98159873+ikechukwuelijah@users.noreply.github.com>
2025-07-24 18:33:23 +0100

    Update query-b.sql

--- query-b.sql
+++ query-b.sql
@@ -1,6 +1,5 @@
 """
-@author: 21076682
-Task 4b. Return the mean values of PM2.5 (particulate matter <2.5 micron diameter) & VPM2.5 (volatile particulate matter <2.5 micron diameter) by each station for the year 2019 for readings taken on or near 08:00 hours (peak traffic intensity)
+Task: Return the mean values of PM2.5 (particulate matter <2.5 micron diameter) & VPM2.5 (volatile particulate matter <2.5 micron diameter) by each station for the year 2019 for readings taken on or near 08:00 hours (peak traffic intensity)
 """
 
 select location.location, AVG(`reading`.`PM2.5`) , AVG(`reading`.`VPM2.5`) from  reading,location
